b0d35b67723cc39103bf61c82a358519a208abd5
VerónicaLondoño <veronicalondonopereira@alumno.ieselrincon.es>
2021-12-24 23:22:18 +0000

    Primer cambio

--- figuras/Test.java
+++ figuras/Test.java
@@ -8,7 +8,7 @@ public class Test {
         Circulo c1 = new Circulo(5.5);
         Circulo c2 = new Circulo(10.1);
         Circulo c3 = new Circulo(10.9);
-
+#probando
    if (c2.esIgual(c3, false))
             System.out.println("c2 y c3: iguales sin considerar decimales");
 
